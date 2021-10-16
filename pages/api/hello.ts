8dd519aa876881762a4f5ec546c75b9ac8ea5839
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-10-16 19:59:27 +0300

    added dynamic route to api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nc from "next-connect";
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'Hello from Next api.' })
-}
+export default nc<NextApiRequest, NextApiResponse<Data>>()
+.get((req, res) => {
+  
+  res.status(200).json({ name: 'Hello from Next api.' });
+});
+
