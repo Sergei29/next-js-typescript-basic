6c5fbb638ea02440e9e5d3ee66f49ca4e83c7ebb
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-10-17 16:17:08 +0300

    contants updated for api url

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,4 @@
-export const API_INDEX_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api"
-    : "https://next-js-typescript-basic.vercel.app/api";
+export const API_INDEX_URL = process.env.NEXT_PUBLIC_API_INDEX_URL;
 export const MONGO_URI = process.env.NEXT_PUBLIC_MONGO_URI;
 export const ENVIRONMENT = process.env.NEXT_PUBLIC_VERCEL_ENV;
 export const HELP_APP_URL = process.env.NEXT_PUBLIC_HELP_APP_URL;
