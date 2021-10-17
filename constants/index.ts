498d42569c60f38c1dc7bd75d952e17a495ddb44
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-10-17 16:10:43 +0300

    env var updated

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,7 @@
 export const API_INDEX_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : "https://next-js-typescript-basic.vercel.app/api";
 export const MONGO_URI = process.env.NEXT_PUBLIC_MONGO_URI;
 export const ENVIRONMENT = process.env.NEXT_PUBLIC_VERCEL_ENV;
 export const HELP_APP_URL = process.env.NEXT_PUBLIC_HELP_APP_URL;
